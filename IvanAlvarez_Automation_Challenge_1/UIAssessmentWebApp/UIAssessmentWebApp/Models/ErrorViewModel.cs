f850f1ed7dc721bb980714898ff1c728b73eb3dc
Ivan <alvarezivan001@yahoo.com>
2019-01-14 13:28:23 -0600

    first

---
+++ IvanAlvarez_Automation_Challenge_1/UIAssessmentWebApp/UIAssessmentWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UIAssessmentWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ IvanAlvarez_Automation_Challenge_1/UIAssessmentWebApp/UIAssessmentWebApp/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UIAssessmentWebApp.Models
+{
+    public class LoginModel
+    {
+        public string firstname { get; set; }
+        public string lastname  { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string address   { get; set; }
+        public string company { get; set; }
+        public string role { get; set; }
+
+
+    }
+}
